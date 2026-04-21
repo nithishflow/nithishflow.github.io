2eb20fe39c5546291ad4278dbe8ce85b0021adf7
nithish kumar <59926590+nithishflow@users.noreply.github.com>
2026-04-21 19:29:03 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
+const elements = document.querySelectorAll('.fade-in, .slide-in');
+
+window.addEventListener('scroll', () => {
+elements.forEach(el => {
+const position = el.getBoundingClientRect().top;
+if (position < window.innerHeight - 100) {
+el.classList.add('show');
+}
+});
+});
 
